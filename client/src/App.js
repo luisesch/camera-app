c7d9a52de8cea2a992253fa9476c8ad232abd163
Luise Schneider <luise.schneider@berlin.de>
2022-03-29 17:13:46 +0200

    update packages client and update react code

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
-import { withRouter } from "react-router";
-import { Switch, Route } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 // import components
 import Welcome from "./components/Welcome";
@@ -11,61 +11,47 @@ import Checkout from "./components/Checkout";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: "",
-      email: ""
-    };
-  }
+function App(props) {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const navigate = useNavigate();
 
   // set state with entered username and email
-  getUser = (name, email) => {
-    this.setState({ username: name, email: email });
-    this.props.history.push("/upload");
+  const getUser = (name, email) => {
+    setUsername(name);
+    setEmail(email);
+    navigate("/upload");
   };
 
-  render() {
-    // make sure there is a username; if not, render homepage
-    if (this.state.username.length === 0) {
-      return (
-        <div className="App">
-          <NavbarComp />
-          <Welcome setUser={this.getUser} />
-        </div>
-      );
-    } else {
-      return (
-        <div className="App">
-          <NavbarComp />
+  // make sure there is a username; if not, render homepage
+  if (username.length === 0) {
+    return (
+      <div className="App">
+        <NavbarComp />
+        <Welcome setUser={getUser} />
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <NavbarComp />
 
-          <Switch>
-            <Route
-              exact
-              path={"/"}
-              component={() => <Welcome setUser={this.getUser} />}
-            />
-            <Route
-              exact
-              path={"/upload"}
-              component={() => (
-                <UploadPage
-                  user={this.state.username}
-                  email={this.state.email}
-                />
-              )}
-            />
-            <Route
-              exact
-              path={"/thank-you"}
-              component={() => <Checkout user={this.state.username} />}
-            />
-          </Switch>
-        </div>
-      );
-    }
+        <Routes>
+          <Route exact path={"/"} element={<Welcome setUser={getUser} />} />
+          <Route
+            exact
+            path={"/upload"}
+            element={<UploadPage user={username} email={email} />}
+          />
+          <Route
+            exact
+            path={"/thank-you"}
+            element={<Checkout user={username} />}
+          />
+        </Routes>
+      </div>
+    );
   }
 }
 
-export default withRouter(App);
+export default App;
--- client/src/components/Checkout.js
+++ client/src/components/Checkout.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { withRouter } from "react-router";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 // import React-bootstrap components
 import Container from "react-bootstrap/Container";
@@ -9,33 +9,33 @@ import Button from "react-bootstrap/Button";
 
 import "./Checkout.css";
 
-class Checkout extends Component {
+function Checkout(props) {
+  const navigate = useNavigate();
+
   // redirect to upload page without refreshing
-  redirect = () => {
-    this.props.history.push("/upload");
+  const redirect = () => {
+    navigate("/upload");
   };
 
-  render() {
-    return (
-      <Container fluid className="container-checkout bg-white p-5">
-        <h1>Thank you, {this.props.user}!</h1>
-        <h4>Your photo has been sent.</h4>
-        <Row>
-          <Col />
-          <Col>
-            <Button
-              className="button-blue px-5 mt-4"
-              type="submit"
-              onClick={this.redirect}
-            >
-              Take another photo
-            </Button>
-          </Col>
-          <Col />
-        </Row>
-      </Container>
-    );
-  }
+  return (
+    <Container fluid className="container-checkout bg-white p-5">
+      <h1>Thank you, {props.user}!</h1>
+      <h4>Your photo has been sent.</h4>
+      <Row>
+        <Col />
+        <Col>
+          <Button
+            className="button-blue px-5 mt-4"
+            type="submit"
+            onClick={redirect}
+          >
+            Take another photo
+          </Button>
+        </Col>
+        <Col />
+      </Row>
+    </Container>
+  );
 }
 
-export default withRouter(Checkout);
+export default Checkout;
